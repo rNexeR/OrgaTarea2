24df1768687c476936ba5ef0e35ebda8ca750ce1
Turupawn <ahmed.hn.43@gmail.com>
2014-11-20 18:21:24 -0600

    primer commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+// queue::push/pop
+#include "Evaluador.h"
+#include <iostream>
+#include <list>
+#include <fstream>
+using namespace std;
+
+//Escribe los datos del objeto mascota dado en un archivo binario con el nombre dado en la posicion dada
+//Nota: Se deben poder agregar varias mascotas en un solo archivo
+void escribir(string nombre_archivo, list<Cumpleanios*>cumpleanios)
+{
+}
+
+//Devuelve una mascota previamente escrita (con la funcion escribir()) en un archivo binario con nombre dado en la posicion dada
+//Nota: Se deben poder leer varias mascotas de un solo archivo
+list<Cumpleanios*> leer(string nombre_archivo)
+{
+    list<Cumpleanios*>cumpleanios;
+    return cumpleanios;
+}
+
+int main ()
+{
+    //Funcion evaluadora
+    evaluar();
+    return 0;
+}
